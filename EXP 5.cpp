8e4760fbaf157712f9c42f37a5fbcb062ccfcc8b
B.Jaswanth Reddy <112232703+192110623@users.noreply.github.com>
2023-11-02 14:35:51 +0530

    SUMMING OF SERIES 1+2+3+4+5

---
+++ EXP 5.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int sum = 0;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    if (n < 1) {
+        printf("Please enter a positive integer.\n");
+        return 1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    printf("Sum of the series 1 + 2 + 3 + ... + %d is: %d\n", n, sum);
+
+    return 0;
+}
+
